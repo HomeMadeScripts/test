6dfaf2223590d9f1ae4bfe30d00a046ff86c682a
HomeMadeScripts <coolsteamacc123@gmail.com>
2025-04-09 01:07:29 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+module.exports = {
+    token: "MTM0MDc3ODczMzM4ODQzMTQ4Mg.G2fadd.2HshbNa1pVn_17QFtUc7J9KU8wa4eSGbv3iZ4U",
+    prefix: "!",
+    allowedUserIDs: ['1340778733388431482', 'yourid2', 'yourid3']
+};
